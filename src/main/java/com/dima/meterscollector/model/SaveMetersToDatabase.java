c6bc8873f740fd941fce48559db6c587ad9ae2c8
dimarafael <dimarafael@gmail.com>
2023-02-23 12:10:34 +0100

    SaveMetersToDatabase add "Ead"

--- src/main/java/com/dima/meterscollector/model/SaveMetersToDatabase.java
+++ src/main/java/com/dima/meterscollector/model/SaveMetersToDatabase.java
@@ -30,6 +30,7 @@ public class SaveMetersToDatabase {
         for(MeterData meterData : meterDataList){
             MetersDb metersDb = new MetersDb();
             metersDb.setEa(meterData.getEa());
+            metersDb.setEad(metersDb.getEad());
             metersDb.setEr(meterData.getEr());
             metersDb.setEg(meterData.getEg());
             metersDb.setEs(meterData.getEs());
