99ca96a1de088b2587128a917d8fab9f5f79dad0
dimarafael <dimarafael@gmail.com>
2023-02-28 10:49:32 +0100

    front: update button on logs page

--- src/main/frontend/src/pages/LogsPage.tsx
+++ src/main/frontend/src/pages/LogsPage.tsx
@@ -1,16 +1,32 @@
 import {useEffect, useState} from "react";
+import {RxUpdate} from 'react-icons/rx'
 
 export function LogsPage(){
     const [log, setLog] = useState('')
 
-    useEffect(()=>{
+    function getLog(){
         fetch('/actuator/logfile')
             .then(response => response.text())
             .then(text => setLog(text))
+    }
+
+    useEffect(()=>{
+        // fetch('/actuator/logfile')
+        //     .then(response => response.text())
+        //     .then(text => setLog(text))
+        getLog()
     },[])
     return(
-        <div className='flex w-full text-left text-sm whitespace-pre-wrap px-2'>
-            {log}
-        </div>
+        <>
+            <div className='flex w-full text-left text-sm whitespace-pre-wrap px-2'>
+                {log}
+            </div>
+            <div className='fixed top-24 right-10 text-4xl p-2 text-neutral-400
+                            hover:shadow active:text-neutral-600'
+                onClick={getLog}
+            >
+                <RxUpdate/>
+            </div>
+        </>
     )
 }
